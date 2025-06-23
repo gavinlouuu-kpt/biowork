968ab62af566ec2f50b60e86163413183b96f472
Sergei Koshevarov <sergey.koshevarov@humansignal.com>
2025-06-23 12:42:21 +0100

    fix: BROS-128: Fix large data multichannel displaying (#7800)
    Co-authored-by: Gondragos <Gondragos@users.noreply.github.com>

--- web/libs/editor/tests/e2e/tests/toggle-visibility.test.js
+++ web/libs/editor/tests/e2e/tests/toggle-visibility.test.js
@@ -181,6 +181,7 @@ Scenario.skip("Checking regions grouped by label", async ({ I, LabelStudio }) =>
   I.click('[data-testid="grouping-manual"]');
   I.waitForElement(".lsf-menu", 2);
 
+  I.waitForElement(".lsf-dropdown.lsf-visible");
   // Click on the "Group by Label" option in the dropdown
   I.click('//div[contains(@class, "lsf-view-controls__label") and contains(text(), "Group by Label")]');
 
