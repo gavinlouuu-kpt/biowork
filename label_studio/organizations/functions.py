45e69e62d6c89c47db3babcc2a7212252a630ce4
gavin <gavinlouuu@gmail.com>
2025-10-04 17:34:05 +1000

    auto enable token

--- label_studio/organizations/functions.py
+++ label_studio/organizations/functions.py
@@ -5,7 +5,7 @@ from organizations.models import Organization, OrganizationMember
 from projects.models import Project
 
 
-def create_organization(title, created_by, legacy_api_tokens_enabled=False, **kwargs):
+def create_organization(title, created_by, legacy_api_tokens_enabled=True, **kwargs):
     from core.feature_flags import flag_set
 
     JWT_ACCESS_TOKEN_ENABLED = flag_set('fflag__feature_develop__prompts__dia_1829_jwt_token_auth')
@@ -16,8 +16,11 @@ def create_organization(title, created_by, legacy_api_tokens_enabled=False, **kw
         if JWT_ACCESS_TOKEN_ENABLED:
             # set auth tokens to new system for new users, unless specified otherwise
             org.jwt.api_tokens_enabled = True
+            # Enable legacy tokens by default for ML backend compatibility
+            # Can be overridden via explicit parameter or environment variable
             org.jwt.legacy_api_tokens_enabled = (
-                legacy_api_tokens_enabled or settings.LABEL_STUDIO_ENABLE_LEGACY_API_TOKEN
+                legacy_api_tokens_enabled if kwargs.get('force_legacy_setting') else
+                (legacy_api_tokens_enabled or settings.LABEL_STUDIO_ENABLE_LEGACY_API_TOKEN)
             )
             org.jwt.save()
         return org
